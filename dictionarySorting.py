7e23fa6e7038cb4db2bbc02b4c73f4adeda19e3f
polarTurtle <76888013+polarTurtle@users.noreply.github.com>
2021-02-28 13:17:52 +1100

    longDictoronaryTestingCompleted

---
+++ dictionarySorting.py
@@ -0,0 +1,24 @@
+def returnOne():
+    return 1
+
+
+def basic_bar(results):
+    listForm = []
+    for key, value in results.items():
+        temp = [key, value]
+        listForm.append(temp)
+    listForm.sort(key=lambda x: (-x[1], x[0]))
+    finalString = ""
+    for quater, amount in listForm:
+        if amount == 0:
+            temp = quater + "|" + "0" + "\n"
+        else:
+            temp = quater + "|" + "#"*int(amount/50) + " " + str(amount) + "\n"
+        finalString += temp
+    return finalString.strip("\n")
+
+
+# sampleDict = {'Q4': 0, 'Q3': 100, 'Q2': 0, 'Q1': 600}
+# print(basic_bar(sampleDict))
+
+# "Q1|############ 600\nQ3|## 100\nQ2|0\nQ4|0"
