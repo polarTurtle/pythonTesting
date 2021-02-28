699b4278b9daf197c0374b48b4a22ce9f88700e5
polarTurtle <76888013+polarTurtle@users.noreply.github.com>
2021-02-28 14:13:50 +1100

    short version

--- dictionarySorting.py
+++ dictionarySorting.py
@@ -18,7 +18,20 @@ def basic_bar(results):
     return finalString.strip("\n")
 
 
-# sampleDict = {'Q4': 0, 'Q3': 100, 'Q2': 0, 'Q1': 600}
+def short_bar(results):
+    listForm = sorted(results.items(), key=lambda x: (-x[1], x[0]))
+    finalString = ""
+    for quater, amount in listForm:
+        if amount == 0:
+            temp = quater + "|" + "0" + "\n"
+        else:
+            temp = quater + "|" + "#"*int(amount/50) + " " + str(amount) + "\n"
+        finalString += temp
+    return finalString.strip("\n")
+
+
+sampleDict = {'Q4': 0, 'Q3': 100, 'Q2': 0, 'Q1': 600}
 # print(basic_bar(sampleDict))
+print(short_bar(sampleDict))
 
 # "Q1|############ 600\nQ3|## 100\nQ2|0\nQ4|0"
